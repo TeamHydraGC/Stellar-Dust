31c1b1f9fe79e19531dbe9e8d1ae3c292350566e
BigVinnerz <ifoundaclue1327@hotmail.com>
2024-10-20 23:03:04 -0400

    Jump mechanic inside of PlayerMovement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,28 +6,51 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private int moveSpeed;
-
+    [SerializeField] private float jumpForce = 10f;
     public bool useTransformMovement;
-    // Start is called before the first frame update
+    private bool isGrounded = false;
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Horizontal Movement
         float x = Input.GetAxis("Horizontal");
 
         if (useTransformMovement == false)
         {
-            rb.linearVelocity = new Vector3(x, rb.linearVelocity.y, 0);
+            rb.linearVelocity = new Vector2(x * moveSpeed, rb.linearVelocity.y);
         }
         else
         {
             transform.position = new Vector3(transform.position.x + x * Time.deltaTime * moveSpeed,
-            transform.position.y, transform.position.z);
+                                             transform.position.y,
+                                             transform.position.z);
         }
 
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Check if the player is on the ground using raycast
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player is grounded
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
     }
 }
