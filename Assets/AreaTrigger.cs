d798b9560ca167665ad1ac909f71474e6bc05a37
BigVinnerz <ifoundaclue1327@hotmail.com>
2024-10-24 17:26:08 -0400

    Added AreaTrigger & PlayerTrigger scripts for TMP.

---
+++ Assets/AreaTrigger.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class AreaTrigger : MonoBehaviour
+{
+    public string title = "Stellar Saloon";
+    public string author = "By: Vincent C";
+
+}
---
+++ Assets/PlayerAreaTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerAreaTrigger : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI titleText;
+    public TMPro.TextMeshProUGUI byLineText;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        AreaTrigger area = collision.GetComponent<AreaTrigger>();
+        
+        titleText.text = area.title;
+        byLineText.text = "By: " + area.author;
+
+    }
+
+}
