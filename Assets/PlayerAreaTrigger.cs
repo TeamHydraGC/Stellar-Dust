f54bcf711f7f8ae087fcc0f7bb0db05843de9eb2
BigVinnerz <ifoundaclue1327@hotmail.com>
2024-11-05 00:32:19 -0500

    reset

--- Assets/PlayerAreaTrigger.cs
+++ Assets/PlayerAreaTrigger.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class PlayerAreaTrigger : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI titleText;
-    public TMPro.TextMeshProUGUI byLineText;
+    //public TMPro.TextMeshProUGUI titleText;
+    //public TMPro.TextMeshProUGUI byLineText;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         AreaTrigger area = collision.GetComponent<AreaTrigger>();
         
-        titleText.text = area.title;
-        byLineText.text = "By: " + area.author;
+        //titleText.text = area.title;
+        //byLineText.text = "By: " + area.author;
 
     }
 
