77fd4a1007bd7a22a685f4cf0c46150551387aba
BigVinnerz <ifoundaclue1327@hotmail.com>
2024-12-13 20:26:44 -0500

    added player respawn and sound

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform respawnPoint;
+    public AudioSource audioSource;
+    public AudioClip respawnSound;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = respawnPoint.position;
+            audioSource.PlayOneShot(respawnSound);
+        }
+    }
+}
