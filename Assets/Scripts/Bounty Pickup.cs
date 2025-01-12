d97efcd0f0387fea2c8665a01b91c5781db4666c
Evan Gentile <ewgentile@gmail.com>
2025-01-12 02:01:57 -0500

    more

--- Assets/Scripts/Bounty Pickup.cs
+++ Assets/Scripts/Bounty Pickup.cs
@@ -4,28 +4,13 @@ using UnityEngine;
 public class BountyPickup : MonoBehaviour
 {
 
-    //private var mouseClick = Input.GetMouseButton(1);
-
-
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
 
     public void OnMouseEnter()
     {
         
-        if  (Input.GetMouseButton(1) == true)
+        if  (Input.GetMouseButtonDown(1) == true)
         {
             Debug.Log("Picked up bounty!");
         }
