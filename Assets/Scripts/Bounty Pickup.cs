048963a34f873ffad2d88936814bd9c2842f2599
Evan Gentile <ewgentile@gmail.com>
2025-01-12 00:29:02 -0500

    started on bounty pickup script

---
+++ Assets/Scripts/Bounty Pickup.cs
@@ -0,0 +1,47 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class BountyPickup : MonoBehaviour
+{
+
+    //private var mouseClick = Input.GetMouseButton(1);
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnMouseEnter()
+    {
+        
+        if  (Input.GetMouseButton(1))
+        {
+            
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
