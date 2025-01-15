348b7563a35f3cfe3e7a2f6ae0355904a61a87f9
echo <auricom@outlook.com>
2025-01-15 17:43:04 -0500

    added comments, oops
    yep

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
 
-    public int playerHealth;
-    public int playerMaxHealth = 4;
+    public int playerHealth; // current player health, do not touch unless via the TakeDamage function below!
+    public int playerMaxHealth = 4; // maximum player health, dont tweak during runtime
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,12 +17,12 @@ public class PlayerHealth : MonoBehaviour
         
     }
 
-    public void TakeDamage(int amount)
+    public void TakeDamage(int amount) // function to deal damage to the player
     {
-        playerHealth -= amount;
-        if (playerHealth <= 0)
+        playerHealth -= amount; // 
+        if (playerHealth <= 0) // player death logic
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // VERY TEMPORARY, DO NOT SHIP THIS
         }
     }
 }
