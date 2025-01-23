19c36813cb479d536b7fbad51e7c06ed7ad25745
echo <auricom@outlook.com>
2025-01-23 11:54:08 -0500

    dying loads GameOver scene

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,12 @@
 // Authored by Nate
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int playerHealth; // current player health, do not touch unless via the TakeDamage function below!
     public int playerMaxHealth = 4; // maximum player health, dont tweak during runtime
+    public bool playerDead = false;
 
     public void playerTakeDamage(int amount) // Method to deal damage to the player
     {
@@ -14,11 +16,13 @@ public class PlayerHealth : MonoBehaviour
         {
             Debug.Log("Player health is at 1! Taking any more damage will kill the player!");
         }
-        
-        if (playerHealth <= 0) // player death logic, DO NOT SHIP THIS
+
+        if (playerHealth <= 0) // if player is dead, set playerDead to true; will influence the next if statement
         {
-            Debug.Log("Player health <= 0, destroying player! THIS BREAKS A LOT OF THINGS, DO NOT SHIP!");
-            Destroy(gameObject); // VERY TEMPORARY, DO NOT SHIP THIS
+            Debug.Log("Player health <= 0, creating respawn dialogue.");
+            playerDead = true;
+            SceneManager.LoadScene(sceneBuildIndex: 2);
+            playerDead = !playerDead;
         }
     }
 }
