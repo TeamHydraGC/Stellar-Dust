8723d8376a4af7664c48e976d7d6bad9c295ca6e
Robobanks12 <alex.macgregor@rogers.com>
2025-01-30 17:57:55 -0500

    Started the Falling Platform script and the Elevator Platform script
    I can move the location of the platforms if needed.

---
+++ Assets/Scripts/Mechanic/ElevatorPlatform.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class ElevatorPlatform
+{
+    // The platfrom will being going up and down at a set speed
+}
---
+++ Assets/Scripts/Mechanic/FallingPlatforms.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class FallingPlatforms
+{
+    // Make the plaforms stay put and only fall when the player moves jump on them, though they will only fall after a second of staying on the platfrom (like Mega Man 5's Gyro Man stage)
+}
