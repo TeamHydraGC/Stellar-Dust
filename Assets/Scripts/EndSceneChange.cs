2ce235652e079f98b2e24d49bedd696ca79abd13
Evan Gentile <ewgentile@gmail.com>
2025-01-31 17:19:46 -0500

    tweaked lights

--- Assets/Scripts/EndSceneChange.cs
+++ Assets/Scripts/EndSceneChange.cs
@@ -3,22 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneChange : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
+    
     void OnCollisionEnter2D()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(4);
     }
 
 
