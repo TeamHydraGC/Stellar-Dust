092204957a5ae64eed1e6a063dc6a8cb3f748f20
echo <auricom@outlook.com>
2025-01-31 17:44:47 -0500

    enemies get deleted when hp = 0

--- Assets/Scripts/Enemy/ContactDamage.cs
+++ Assets/Scripts/Enemy/ContactDamage.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 public class ContactDamage : MonoBehaviour
 {
     public PlayerHealth playerHealth; // import playerHealth, ATTACH PLAYERHEALTH TO OBJECT IN EDITOR
-    public int damageValue = 1; // how much damage the object this script is attached to deals, can be tweaked in-editor
+    public int damageValue = 1; // how much damage the object this script is attached to deals, can be tweaked in-editor. havent tested tweaking during runtime.
 
     private void OnTriggerEnter2D(Collider2D other) // detecting collission
     {
-        if(other.gameObject.tag == "Player") // if colliding with an object tagged as Player, do:
+        if(other.gameObject.tag == "Player") // if colliding with an object tagged as Player, apply damageValue amount of damage
         {
             playerHealth.playerTakeDamage(damageValue);
-            Debug.Log("Dealt " + damageValue + " to player.");
+            Debug.Log(gameObject.name + " has dealt " + damageValue + " damage.");
+            
             //Task.Delay(250); // This is a hack, being replaced by the InvulnAfterDamageTaken coroutine in PlayerHealth.cs. Re-enable only if shit really goes sideways.
         }
     }
--- Assets/Scripts/Mechanic/Scoring.cs
+++ Assets/Scripts/Mechanic/Scoring.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Scoring : MonoBehaviour
 {
     // Declaring variables
-    public static int currentScore; // Current score, can be added or subtracted from
+    public int currentScore; // Current score, can be added or subtracted from
     public static int totalScore; // Total score, DO NOT TOUCH. I'll find a way to make this write-only later.
 
     private void Start()
