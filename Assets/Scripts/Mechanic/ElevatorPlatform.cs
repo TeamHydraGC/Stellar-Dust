c4493f85983b3d170a29d32f99938418e3a1926a
A.J. <200609705@student.georgianc.on.ca>
2025-02-01 21:51:21 -0500

    Added Elevator Platform script again and applied it to the platform

---
+++ Assets/Scripts/Mechanic/ElevatorPlatform.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class ElevatorPlatform : MonoBehaviour
+{
+    public float maxHeight = -1.00f;
+
+    public float minHeight = -4.44f;
+
+    public float speedUp = 0.8f;
+
+    public float speedDown = 0.5f;
+
+    private float step = 0.0f;
+
+    private bool movingUp = true;
+
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 currentPostion = rb.position;
+
+        if (movingUp)
+        {
+            step = speedUp * Time.fixedDeltaTime;
+            currentPostion.y += step;
+
+            if (currentPostion.y >= maxHeight)
+            {
+                currentPostion.y = maxHeight;
+                movingUp = false;
+            }
+
+        }
+        else
+        {
+            step = speedDown * Time.fixedDeltaTime;
+            currentPostion.y -= step;
+
+            if (currentPostion.y <= minHeight)
+            {
+                currentPostion.y = minHeight;
+                movingUp = true;
+            }
+        }
+
+        rb.MovePosition(currentPostion);
+
+    }
+}
