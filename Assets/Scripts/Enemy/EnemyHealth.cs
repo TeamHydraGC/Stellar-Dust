f212c16af9eceb2e641b9ec0c785536ecc030f83
echo <auricom@outlook.com>
2025-02-11 12:35:32 -0500

    ENEMY HEALTH AND DEATH WORKS
    Enemy invulnerability frames are disabled in code right now, BUT IT WORKS

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -10,7 +10,7 @@ public class EnemyHealth : MonoBehaviour
     public int enemyHealth; // current enemy health, DO NOT TOUCH UNLESS VIA TakeDamage
     public int enemyMaxHealth; // maximum enemy health, dont tweak during runtime
     public int scoreValue = 1; // Amount of score eliminating the object this script is attached to will award
-    public float enemyInvulnTime = 0.016f; // How long invulnerability should last, as a float
+    public float enemyInvulnTime = 0.0f; // How long invulnerability should last, as a float
     public bool enemyInvulnerable = false; // Used for invulnerability after taking damage, DO NOT TOUCH UNLESS VIA InvulnAfterDamageTaken COROUTINE
     public bool enemyDead = false; // unused for now, will be used to handle death animations in the future
 
@@ -33,18 +33,18 @@ public class EnemyHealth : MonoBehaviour
         if (enemyInvulnerable == false) // if not invulnerable, deal damage
         {
             enemyHealth -= amount; // Decreases enemyHealth by an int given by enemyTakeDamage
-            enemyInvulnerable = !enemyInvulnerable;
-            StartCoroutine(InvulnAfterDamageTaken()); // Starting coroutine for i-frame timer
-            Debug.Log(gameObject.name + " is now invulnerable.");
-        }
-        else if (enemyHealth <= 0) // if health reaches zero, destroy the object
-        {
-            Debug.Log(gameObject.name + " has died, destroying object.");
-            Object.Destroy(gameObject);
+            //enemyInvulnerable = !enemyInvulnerable;
+            //StartCoroutine(InvulnAfterDamageTaken()); // Starting coroutine for i-frame timer
+            //Debug.Log(gameObject.name + " is now invulnerable.");
         }
         else
         {
             Debug.Log(gameObject.name + " health is currently " + enemyHealth);
+        }
+        if (enemyHealth <= 0) // if health reaches zero, destroy the object
+        {
+            Debug.Log(gameObject.name + " has died, destroying object.");
+            Object.Destroy(gameObject);
         }
 
         //Debug logging separate from health logic
@@ -52,14 +52,14 @@ public class EnemyHealth : MonoBehaviour
         {
             Debug.LogWarning(gameObject.name + " is at 1 health!");
         }
-        IEnumerator InvulnAfterDamageTaken() // i-frame shit SPECIFIC TO THE ENEMY THIS SCRIPT IS ATTACHED TO 
-        {
-            if (enemyHealth >= 0)
-            {
-                yield return new WaitForSeconds(enemyInvulnTime);
-                enemyInvulnerable = !enemyInvulnerable;
-                Debug.Log(gameObject.name + " now vulnerable.");
-            }
-        }
+        //IEnumerator InvulnAfterDamageTaken() // i-frame shit SPECIFIC TO THE ENEMY THIS SCRIPT IS ATTACHED TO 
+        //{
+        //    if (enemyHealth >= 0)
+        //    {
+        //        yield return new WaitForSeconds(enemyInvulnTime);
+        //        enemyInvulnerable = !enemyInvulnerable;
+        //        Debug.Log(gameObject.name + " now vulnerable.");
+        //    }
+        //}
     }
 }
--- Assets/Scripts/Weapon/BulletScript.cs
+++ Assets/Scripts/Weapon/BulletScript.cs
@@ -8,7 +8,7 @@ public class BulletScript : MonoBehaviour
     private Vector3 mousepos;
     private Camera maincam;
     private Rigidbody2D rb;
-    public EnemyHealth enemyHealth;
+
     public float force; // effectively the bullet's speed
     public int damageValue = 1;
 
@@ -28,18 +28,14 @@ public class BulletScript : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
-    void Update()
-    {
-        //why is this here
-    }
-
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.GetComponent<EnemyHealth>())
         {
-            
-            enemyHealth.enemyTakeDamage(damageValue);
-            Debug.Log("Dealt " + damageValue + " to " + gameObject.name);
+            EnemyHealth hp = collision.gameObject.GetComponent<EnemyHealth>();
+
+            hp.enemyTakeDamage(damageValue);
+            Debug.Log("Dealt " + damageValue + " to " + collision.gameObject.name);
         }
         Destroy(gameObject);
     }
@@ -49,8 +45,4 @@ public class BulletScript : MonoBehaviour
     //{
     //    Destroy(gameObject);
     //}
-
-
-
-
 }
