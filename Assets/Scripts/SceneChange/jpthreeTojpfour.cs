f3b2b2dfeed992612b641de18a5a982ca2d9742b
Robobanks12 <alex.macgregor@rogers.com>
2025-03-05 15:42:53 -0500

    Added a template for the scene change from jp 3 to 4
    I //'d them out at the moment cus Unity was having an aneurism when they weren't. We can put in there when the 3rd area's layout is finished. Just copied what the other 2 scene changes did, may require additional edits.

---
+++ Assets/Scripts/SceneChange/jpthreeTojpfour.cs
@@ -0,0 +1,19 @@
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class jptwoTOjpthree : MonoBehaviour
+//{
+//    private void OnCollisionEnter2D(Collision2D collision)
+//    {
+        //Change the scene to the outdoors
+
+//        if (collision.transform.name == "Player")
+//        {
+//            SceneManager.LoadScene(10);
+//        }
+
+
+
+
+//    }
+//}
