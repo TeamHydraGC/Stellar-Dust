865137ec7022f9d50d9090a19605a1dd78439513
A.J. <200609705@student.georgianc.on.ca>
2025-03-28 20:54:27 -0400

    Added shop scene and button handler

---
+++ Assets/Scripts/Shop/ButtonHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public Button damageButton; // Reference to the button
+    private bool buttonPressed = false; // Ensure the button is pressed only once
+
+    void Start()
+    {
+        // Make sure the button is not pressed initially
+        damageButton.onClick.AddListener(OnButtonClick);
+    }
+
+    void OnButtonClick()
+    {
+        if (!buttonPressed)
+        {
+            buttonPressed = true;
+            BulletScript[] bullets = FindObjectsOfType<BulletScript>();
+
+            foreach (BulletScript bullet in bullets)
+            {
+                bullet.damageValue *= 2;
+                Debug.Log("Bullet damage multiplied by 2!");
+            }
+
+            damageButton.interactable = false;
+        }
+    }
+}
