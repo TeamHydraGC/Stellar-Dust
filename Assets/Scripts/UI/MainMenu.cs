22d8a084ccb37fef6f43f626e37740dbf37e24f1
BigVinnerz <ifoundaclue1327@hotmail.com>
2025-04-01 13:04:49 -0400

    changed build profile to RISE DEMO only scenes, added canvas/UI to RISE scene
    made play button from main menu play to RISE Scene
    added gore option to options in main menu | adjusted size of gore button

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(4);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
